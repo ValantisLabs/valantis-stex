e0eae90401707c30c6eb68a241170e6dda75d541
happenwah <tiagolixa@hotmail.com>
2025-02-03 12:05:17 +0000

    chore: add MIT license to interfaces and mocks

--- src/interfaces/ISTEXRatioSwapFeeModule.sol
+++ src/interfaces/ISTEXRatioSwapFeeModule.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
 import {ISwapFeeModuleMinimal} from "@valantis-core/swap-fee-modules/interfaces/ISwapFeeModule.sol";
